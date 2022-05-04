5e9d8d2cef0dbe2099b983ce57fc8cad47f398e1
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-03 19:46:20 -0500

    video 114 detalle pelicula

--- back-end/DTOs/PeliculasDTO.cs
+++ back-end/DTOs/PeliculasDTO.cs
@@ -13,7 +13,7 @@ namespace back_end.DTOs
         public string Resumen { get; set; }
         public string Trailer { get; set; }
         public bool EnCines { get; set; }
-        public DateTime FechaLanzamineto { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
         public string Poster { get; set; }
         public List<GenerosDTO> Generos { get; set; }
         public List<PeliculaActorDTO> Actores { get; set; }
