84faf91f42e52193cbe6ea533750aa545dbf6e01
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-17 08:53:12 -0500

    video 126 configuracion identity

--- back-end/Startup.cs
+++ back-end/Startup.cs
@@ -7,18 +7,21 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NetTopologySuite;
 using NetTopologySuite.Geometries;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace back_end
@@ -73,7 +76,27 @@ namespace back_end
                     });
                 });
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                 .AddDefaultTokenProviders();
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opciones =>
+            opciones.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,// no use un toquen de manera indefinida, ejmplo un dia
+                ValidateIssuerSigningKey = true,//validar la firma con la llave privada
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(Configuration["llavejwt"])),
+                ClockSkew = TimeSpan.Zero//no tenga diferencias de tiempo si el token vencio
+            });
+
+
+
             services.AddTransient<IRepositorioEnMemoria, RepositorioEnMemoria>();
             services.AddTransient<MiFiltroDeAccion>();
 
