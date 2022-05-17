98eac2e67fb45b99512485dcef6231c58cacc482
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-17 09:21:00 -0500

    video 127 creando el cuentas contrlooer login, crear usuario y crear token

---
+++ back-end/DTOs/RatingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_end.DTOs
+{
+    public class RatingDTO
+    {
+        public int PeliculaId { get; set; }
+        [Range(1, 5)]
+        public int Puntuacion { get; set; }
+    }
+}
